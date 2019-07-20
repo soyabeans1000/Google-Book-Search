249e984a26ed27e9f6115e1d43a6a70d692a6032
Sarika <sarika@ngoyal.com>
2019-07-19 23:03:00 -0700

    Server routes

--- client/src/pages/Favorites/Favorites.js
+++ client/src/pages/Favorites/Favorites.js
@@ -1,44 +1,48 @@
-import React from 'react'
+import React, {Component} from 'react'
 import axios from 'axios'
 
 
-const Favorites = props => {
+class Favorites extends Component  {
 
 
+  state = {
+    books: []
+  }
+
+  componentDidMount = _ => {
+    axios.get(`/favs`)
+      .then(({ data: books }) => this.setState({
+        books: books
+      }))
+      .catch(e => console.error(e))
+  }
+
+  
+render(){
 
 
   return (
     <>
       <div className="jumbotron">
         <h1 className="display-4">My Favorites</h1>
-      </div>
-      {
-
-// axios.get(`/favs`)
-// .then(({ data: books }) => {
-
-
-//   books.map(book => (
-//     <div className="card" style={{ width: '18rem', margin: 'auto', marginTop: '30px', marginBottom: '30px' }}>
-//       {/* <img className="card-img-top" src={book.link} alt="movie" />
-//       <div className="card-body">
-//         <h5 className="card-title">{book.title}</h5>
-//         <p className="card-text">{book.title}</p> */}
-// Books
-
-//       {/* </div> */}
-//     </div>
-//   ))
- 
-// })
-// .catch(e => console.error(e))
-
-
-        }
-
-        this is favs
+      </div>    
+{
+  this.state.books.map((book, index) => (
+    <div>
+    <div className="card" style={{ width: '18rem', margin: 'auto', marginTop: '30px', marginBottom: '30px' }}> Books </div>
+       <img className="card-img-top" src={book.link} alt="movie" />
+      <div className="card-body">
+        <h5 className="card-title">{book.title}</h5>
+        <p className="card-text">{book.title}</p>        
+        </div>
+        </div>
+        ))
+  }
     </>
   )
+
+}
+ 
 }
 
 export default Favorites
