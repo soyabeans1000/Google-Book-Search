53f51513cb3c3d3c1f1d10aac9a13c6b5a674078
Sarika <sarika@ngoyal.com>
2019-07-22 23:49:53 -0700

    Delete is working

--- client/src/pages/Favorites/Favorites.js
+++ client/src/pages/Favorites/Favorites.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react'
-import axios from 'axios'
-
 
 class Favorites extends Component  {
-
 
   state = {
     books: []
@@ -18,8 +15,6 @@ class Favorites extends Component  {
       this.setState({
         books: articles
       })
-
-
       console.log(this.state.books)
 
     })
@@ -27,23 +22,23 @@ class Favorites extends Component  {
   }
 
 
-  // deleteFav(event)
-  // {
-  //   let newValues = {}
-  //   fetch(`/favs/${event.id}`, {
-  //     method: 'DELETE',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify(newValues)
-  //   })
-  //     .then(_ => {
-  //       console.log('Item deleted')
-  //       // this.getFavs()
-  //     })
-  //     .catch(e => console.error(e))
+  deleteFav = event => {
+    let newValues = {}
+    fetch(`/favs/${event.target.id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newValues)
+    })
+      .then(_ => {
+        console.log('Item deleted')
+      })
+      .catch(e => console.error(e))
 
-  // }
+      this.getFavs()
+
+  }
 
   componentDidMount = _ => {
       this.getFavs()
@@ -58,36 +53,36 @@ render(){
       <div className="jumbotron">
         <h1 className="display-4">My Favorites</h1>
       </div>    
-      </>)
+      
  
-//   this.state.books.map((book, index) => (
-//     <div className="card mb-3">
-//     <div className="row no-gutters">
-//       {/* <div className="col-md-2">
-//       {book.volumeInfo.imageLinks !== undefined ? (
-//         <img src={book.volumeInfo.imageLinks.thumbnail} width="150" height="150" alt=" " />
-//         ) : null}
-//       </div> */}
-//       <div className="col-md-10">
-//         <div className="card-body">
-//           <h5 className="card-title">{book.title}</h5>
-//           <p className="card-text">{book.textSnippet}</p>
-//           {/* <p className="card-text"><small class="text-muted"><a href={book.volumeInfo.previewLink}>Preview Link</a></small> */}
-//         <p> <button className="btn btn-primary" id={book._id} onClick={this.deleteFav}>Add to Favorites</button></p>
-//           {/* </p> */}
+{   this.state.books.map((book, index) => (
+    <div className="card mb-3">
+    <div className="row no-gutters">
+      {/* <div className="col-md-2">
+      {book.volumeInfo.imageLinks !== undefined ? (
+        <img src={book.volumeInfo.imageLinks.thumbnail} width="150" height="150" alt=" " />
+        ) : null}
+      </div> */}
+      <div className="col-md-10">
+        <div className="card-body">
+          <h5 className="card-title">{book.title}</h5>
+          <p className="card-text">{book.textSnippet}</p>
+          {/* <p className="card-text"><small class="text-muted"><a href={book.volumeInfo.previewLink}>Preview Link</a></small> */}
+        <p> <button className="btn btn-primary" id={book._id} onClick={this.deleteFav}>Delete From Favorites</button></p>
+          {/* </p> */}
          
-//         </div> 
-//       </div>
-//     </div>
-//   </div>
-//         ))
-//   }
-  //   </>
-  // )
+        </div> 
+      </div>
+    </div>
+  </div>
+        ))
+  }
+    </>
+  )
 
-//}
- 
 }
+ 
+
 
 }
 
